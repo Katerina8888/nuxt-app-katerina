345308bfbdb2d3532ed33d645110ef66bbd8f941
katerina <katerina@igloonet.cz>
2025-02-14 10:47:37 +0100

    style: improve layout of ProductCard component for better visual hierarchy

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="card text-center">
-    <img :src="product.image" alt="product thumb" class="thumb" />
-    <p class="font-bold text-gray-500 m-4 truncate">{{ product.title }}</p>
-    <NuxtLink :to="`/products/${product.id}`">
-      <p class="btn my-4">View Details</p>
-    </NuxtLink>
+  <div class="card text-center flex flex-col justify-between">
+    <div>
+      <img :src="product.image" alt="product thumb" class="thumb" />
+    </div>
+    <div>
+      <p class="font-bold text-gray-500 m-4 truncate">{{ product.title }}</p>
+      <NuxtLink :to="`/products/${product.id}`">
+        <p class="btn my-4">View Details</p>
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
